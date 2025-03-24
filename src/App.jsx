04118a5f9c71a85f0b845a12c6ef6f420fb2e37b
Shahin Ahmed <shahinahmed7165@gmail.com>
2025-03-24 21:28:49 +0600

    name

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom"; // Remove BrowserRouter import
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Navbar from "./components/Navbar/Navbar";
-import Banner from "./components/Banner/banner";
-import Hero from "./components/Hero/hero";
-import Products from "./components/Product/products";
-import TopProducts from "./components/TopProduct/TopProducts";
-import Popup from "./components/Popup/Popup";
-import Footer from "./Fotter/Fotter";
-import Bestproduct from "./components/BestProduct/Bestproduct";
-import Womenwear from "./components/Pages/Women-wear/womenwear";
-import Womenallproduct from "./components/Pages/Women-wear/womenallproduct"
-import ElectronicsProductList from "./components/Pages/Electronics/ElectronicsProductList";
-import KidsWearProductList from "./components/Pages/kids-wear/kidswear";
+import Navbar from "./sections/Navbar/Navbar";
+import Banner from "./sections/Banner/banner";
+import Hero from "./sections/Hero/hero";
+import Products from "./sections/Product/products";
+import TopProducts from "./sections/TopProduct/TopProducts";
+import Popup from "./sections/Popup/Popup";
+import Footer from "./sections/Fotter/Fotter";
+import Bestproduct from "./sections/BestProduct/Bestproduct";
+import Womenwear from "./sections/Pages/Women-wear/womenwear";
+import Womenallproduct from "./sections/Pages/Women-wear/womenallproduct"
+import ElectronicsProductList from "./sections/Pages/Electronics/ElectronicsProductList";
+import KidsWearProductList from "./sections/Pages/kids-wear/kidswear";
 
 const App = () => {
   const [orderPopup, setOrderPopup] = useState(false);
